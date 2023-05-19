5b48cfc7c399adb4dd07a1d489e331f5274215fb
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-05-19 18:54:16 +0600

    asd
    asd

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@
             domains: ['i.pravatar.cc', "static.wikia.nocookie.net"],
           },
           env: {
-            BACKEND: "https://chatcord-xp6j.onrender.com/",
-            // BACKEND: "http://192.168.31.12:5000",
+            // BACKEND: "https://chatcord-xp6j.onrender.com/",
+            BACKEND: "http://192.168.31.12:5000",
           }
     }
 
