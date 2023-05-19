a3ed9a374a8030ca0c5a8d60c1e2af2c9a5d427e
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-05-19 19:57:33 +0600

    asdas
    dasdas

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
-   
-   
-   /** @type {import('next').NextConfig} */
-    const nextConfig = {
-        images: {
-            domains: ['i.pravatar.cc', "static.wikia.nocookie.net"],
-          },
-          env: {
-            BACKEND: "https://chatcord-xp6j.onrender.com/",
-            // BACKEND: "http://192.168.31.12:5000",
-          }
-    }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["i.pravatar.cc", "static.wikia.nocookie.net, res.cloudinary.com"],
+  },
+  env: {
+    BACKEND: "https://chatcord-xp6j.onrender.com/",
+    // BACKEND: "http://192.168.31.12:5000",
+  },
+};
 
-    module.exports = nextConfig
+module.exports = nextConfig;
